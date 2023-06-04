ce2a08f5ec05dfd0f720d62e8bebedb757ccf346
AkoruuDev <akoruu.dev@gmail.com>
2023-06-04 20:10:05 -0300

    FEAT: Update structure and paths from App

--- idente-front/src/App.js
+++ idente-front/src/App.js
@@ -1,6 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import SignIn from './pages/log/SignIn.js'
+import SignUp from './pages/log/SignUp.js';
+import Home from './pages/contents/Home.js';
+
 function App() {
   return (
-    <></>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <SignIn />} />
+          <Route path='/sign-up' element={ <SignUp />}  />
+
+          <Route path='/home' element={ <Home /> } />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
