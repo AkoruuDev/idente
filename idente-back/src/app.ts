5b43a7639a85192cecb3ceec5b9c1788192d5248
AkoruuDev <akoruu.dev@gmail.com>
2023-06-12 12:45:03 -0300

    FEAT: Update app file

--- idente-back/src/app.ts
+++ idente-back/src/app.ts
@@ -1,9 +1,18 @@
 import { loadEnv, connectDb } from "./config";
 import express, { Express } from "express";
+import cors from 'cors';
+import { clientRoute, userRoute } from "./routes";
 
 loadEnv()
 
 const app = express();
+
+app
+    .use(cors())
+    .use(express.json())
+    .get('health', (_req, res) => res.send('OK!'))
+    .use('/user', userRoute)
+    .use('/client', clientRoute)
 
 export function init(): Promise<Express> {
     connectDb();
